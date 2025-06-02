9250320582a2eaab222523efb45fde178f3ab78b
hieukx <hieukimxuan@gmail.com>
2025-06-02 13:18:42 +0700

    test

---
+++ MainApp.java
@@ -0,0 +1,10 @@
+
+import Control.UserControl;
+import java.io.IOException;
+
+public class MainApp { 
+    public static void main(String[] args) throws IOException {
+        UserControl userControl=new UserControl();
+        userControl.runMenu();
+    }
+}
